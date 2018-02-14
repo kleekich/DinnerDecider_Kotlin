a9d13a85e2ca8b06adc578e6d68007e741e02ac1
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-13 20:09:13 -0800

    selectedFoodTxt ID corrected, foodList array created, and setOnclickListender on decideBtn declared

--- app/src/main/java/com/example/dinnerdecider_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdecider_kotlin/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.dinnerdecider_kotlin
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinese", "Hamburger", "Pizza", "Chipolte", "McDonalds");
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        seledctedFoodTxt.text = "Burger King"
+        selectedFoodTxt.text = "Burger King"
+
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
 
 
     }
